7b07bc67bdd633e7cd9a31b8a4b2bc6bf40e4a3a
Vlad <garonum@gmail.com>
2020-02-18 22:14:20 +0300

    Initial Commit

---
+++ RayNutrition_1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RayNutrition_1.0
+//
+//  Created by LA'D on 18.02.2020.
+//  Copyright © 2020 Garonum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
